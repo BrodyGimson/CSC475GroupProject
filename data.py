2d24850e9f8d354e77b4fa1779424ed3f421c93a
BrodyGimson <brody.gimson@gmail.com>
2024-04-04 11:16:26 -0700

    Refactor data.py to work with genre tags

--- data.py
+++ data.py
@@ -4,15 +4,15 @@ import pandas as pd
 
 import os, random
 
-# TODO: Update dataset creation to incorporate metadata and tags.
-
 # Combines files from the dataset to create input expected for the LSTM.
 #
 # Input:
-#   effect - String
-#       Effect name, must be same as folder name in dataset.
-#   metadata_path - String
-#       Path to metadata csv file.
+#   genre - String
+#       Genre name, must be same from list in effectData.csv name.
+#   effect_data_path - String
+#       Path to effect data csv file.
+#   file_data_path - String
+#       Path to file data csv file.
 #   mu_comp - Boolean - Default True
 #       Specify whether to use mu's law compression.
 #   srate - Integer - Default 22050
@@ -31,26 +31,48 @@ import os, random
 #   effect_audio - np.array
 #       Long audio with applied effect to be used as training and 
 #       testing output.
-def create_data(effect, metadata_path, mu_comp=True, srate=22050, duration=120, type="scale", incl_poly=False):
+def create_data(genre, effect_data_path, file_data_path, mu_comp=True, srate=22050, duration=120, type="scale", incl_poly=False):
     mono_sample_path = os.path.join("dataset", "monophonic", "Samples")
 
     if incl_poly == True:
         print("Polymphonic not currently supported. Ignoring param.")
     
-    clean_path = os.path.join(mono_sample_path, "NoFX")
-    effect_path = os.path.join(mono_sample_path, effect)
+    effect_df = pd.read_csv(effect_data_path)
+    file_df = pd.read_csv(file_data_path)
 
-    if (not os.path.isdir(effect_path)):
-        print("Effect could not be found, double check string format.")
+    effect_df = effect_df[( effect_df.genre == genre)].iloc[0]
+
+    if (effect_df.empty):
+        print("Genre could not be found, double check string format.")
         return [], []
 
+    clean_path = os.path.join(mono_sample_path, "NoFX")
+    effect_path = os.path.join(mono_sample_path, effect_df.fxType)
+
+    # Only grab files that match the genre given
+    file_effect_df = file_df[
+        ( file_df.playStyle == 3 ) &
+        ( file_df.fxType == effect_df.fxTypeID ) &
+        ( file_df.fxSetting == effect_df.fxSettingID )
+    ]
+
+    # Only select picking style and no effect
+    file_clean_df = file_df[
+        ( file_df.playStyle == 3 ) &
+        ( file_df.fxType == 11 )
+    ]
+
     if (type == "scale"):
-        clean_audio = _create_scale(clean_path, metadata_path, srate, duration)
-        effect_audio = _create_scale(effect_path, metadata_path, srate, duration)
+        # Duration / 2 makes sure a scale of specified duration is guaranteed.
+        # 2 is used because the dataset notes are all 2 second durations.
+        start = random.randint(0, (len(file_clean_df) - (int) (duration / 2)))
+
+        clean_audio = _create_scale(clean_path, file_clean_df, srate, duration, start)
+        effect_audio = _create_scale(effect_path, file_effect_df, srate, duration, start)
     elif (type == "random"):
-        clean_audio, effect_audio = _create_random(clean_path, effect_path, metadata_path, srate, duration)
+        clean_audio, effect_audio = _create_random(clean_path, effect_path, file_clean_df, file_effect_df, srate, duration)
     elif (type == "pentatonic"):
-        clean_audio, effect_audio = _create_pentatonic(clean_path, effect_path, metadata_path, srate, duration)
+        clean_audio, effect_audio = _create_pentatonic(clean_path, effect_path, file_clean_df, file_effect_df, srate, duration)
     else:
         print("Type not found. Select from: scale, random, pentatonic.")
         clean_audio, effect_audio = []
@@ -64,24 +86,17 @@ def create_data(effect, metadata_path, mu_comp=True, srate=22050, duration=120, 
 # -----------------------------------------------------------------------
 # Helper function below are designed to be used only in this module.
 
-# Puts notes in order, up to the duration, into one audio stream.
-def _create_scale(audio_path, metadata_path, srate, duration):
+# Picks a random starting note, then puts notes in order up to a duration.
+def _create_scale(audio_path, file_df, srate, duration, start):
     audio = []
 
     max_length = duration * srate
 
-    file_paths = sorted(os.listdir(audio_path))
+    for _, row in file_df[start:].iterrows():
+        file = os.path.join(audio_path, row.fileID + ".wav")
+        note, _ = lib.load(file, sr=srate)
 
-    # TODO: Update logic to incorporate tagging and fix below.
-    # i counter and modulo 3 are temporary measures.
-    for i, file_path in enumerate(file_paths):
-
-        # Account for the three effect strengths.
-        if (i % 3 == 0 or "NoFX" in audio_path):
-            file = os.path.join(audio_path, file_path)
-            note, _ = lib.load(file, sr=srate)
-
-            audio = np.hstack([audio, note])
+        audio = np.hstack([audio, note])
 
         if (len(audio) >= max_length):
             return audio
@@ -89,26 +104,30 @@ def _create_scale(audio_path, metadata_path, srate, duration):
     return audio
 
 # Chooses a random file each time till the duration of audio is achieved.
-def _create_random(clean_path, effect_path, metadata_path, srate, duration):
+def _create_random(clean_path, effect_path, file_clean_df, file_effect_df, srate, duration):
     clean_audio = []
     effect_audio = []
 
     max_length = duration * srate
 
-    clean_file_paths = sorted(os.listdir(clean_path))
-    effect_file_paths = sorted(os.listdir(effect_path))
-
     while len(clean_audio) < max_length:
-        index = random.randint(0, len(clean_file_paths))
+        index = random.randint(0, len(file_clean_df) - 1)
 
-        clean_file = os.path.join(clean_path, clean_file_paths[index])
+        clean_file = os.path.join(clean_path, file_clean_df.iloc[index].fileID + ".wav")
+        effect_file = os.path.join(effect_path, file_effect_df.iloc[index].fileID + ".wav")
 
-        # TODO: Update with tags
-        # Use *3 to deal with the 3 effect strengths
-        effect_file = os.path.join(effect_path, effect_file_paths[index*3])
+        clean, _ = lib.load(clean_file, sr=srate)
+        effect, _ = lib.load(effect_file, sr=srate)
 
-        clean_note, _ = lib.load(clean_file, sr=srate)
-        effect_note, _ = lib.load(effect_file, sr=srate)
+        # Create random duration
+        note_length = (int) (len(clean) / random.randint(1, 4))
+
+        # Remove leading and trailing silence in a predictable manner
+        clean, indexes = lib.effects.trim(clean)
+        effect = effect[indexes[0]:indexes[1]]
+
+        clean_note = process_note(clean, note_length, srate)
+        effect_note = process_note(effect, note_length, srate)
 
         clean_audio = np.hstack([clean_audio, clean_note])
         effect_audio = np.hstack([effect_audio, effect_note])
@@ -119,7 +138,7 @@ def _create_random(clean_path, effect_path, metadata_path, srate, duration):
 # Chooses a random starting value on the low E string from the files, then maps out 
 # the appropriate pentatonic scale.
 # From that scale it chooses random notes to create the audio up to the duration.
-def _create_pentatonic(clean_path, effect_path, metadata_path, srate, duration):
+def _create_pentatonic(clean_path, effect_path, file_df, srate, duration):
     clean_audio = []
     effect_audio = []
 
@@ -127,3 +146,16 @@ def _create_pentatonic(clean_path, effect_path, metadata_path, srate, duration):
 
     print("Pentatonic type not implemented yet.")
     return []
+
+# Cleans up notes and changes their duration.
+def process_note(note, note_length, srate):
+    fade_length = (int) (srate * 0.1)
+    fade_curve = np.linspace(1.0, 0.0, fade_length)
+
+    note = note[:note_length]
+    fade_start = len(note) - fade_length
+
+    # Add quick fade to make note change less abrupt
+    note[fade_start:] = note[fade_start:] * fade_curve
+
+    return note
