d8518d089ebc1f512a907e62f5b0d93a45d01637
BrodyGimson <brody.gimson@gmail.com>
2024-04-18 12:12:14 -0700

    Clean up and fix

--- model_utils.py
+++ model_utils.py
@@ -71,7 +71,7 @@ def normalized_esr(true, predicted):
 
 # Change data to format expected by the model
 # Non-sequential version
-def create_dataset(input_file, output_file, size_training, size_test, frame, sr, test_ratio, label=None):
+def create_dataset(input_file, output_file, size_training, size_test, frame, sr, test_ratio):
     signal = input_file
     wet = output_file
     
@@ -96,7 +96,7 @@ def create_dataset(input_file, output_file, size_training, size_test, frame, sr,
 
     # Creating the training set (randomly pulling frames and target value from all segments in train set)
     features_train = np.zeros((size_training, frame))
-    targets_train = np.zeros((size_training, frame))
+    targets_train = np.zeros((size_training))
     counter = 0
 
     for i in range(size_training):
@@ -107,12 +107,12 @@ def create_dataset(input_file, output_file, size_training, size_test, frame, sr,
         features = dry_slice[random_start:random_start+frame]
         target = wet_slice[random_start+frame-1]
 
-        features_train[counter,:], targets_train[counter,:] = features, target
+        features_train[counter,:], targets_train[counter] = features, target
         counter += 1
 
     # Creating the testing set (randomly puling frames and target value from segments in test set)
     features_test = np.zeros((size_test, frame))
-    targets_test = np.zeros((size_test, frame))
+    targets_test = np.zeros((size_test))
     counter = 0
 
     for i in range(size_test):
@@ -123,7 +123,7 @@ def create_dataset(input_file, output_file, size_training, size_test, frame, sr,
         features = dry_slice[random_start:random_start+frame]
         target = wet_slice[random_start+frame-1]
 
-        features_test[counter,:], targets_test[counter,:] = features, target
+        features_test[counter,:], targets_test[counter] = features, target
         counter += 1
 
     return dry_test, wet_test, features_train, features_test, targets_train, targets_test
